61854ad8d45d787ca9939b6fbfb9d7f762a1c182
Nikolai <kalistratov@live.com>
2017-12-17 12:21:48 +0100

    readme update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,17 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBsuxDvEWtq0l0xyA9MmBf9Dy_Z0fY3w_M",
+    authDomain: "authenticator-9d301.firebaseapp.com",
+    databaseURL: "https://authenticator-9d301.firebaseio.com",
+    projectId: "authenticator-9d301",
+    storageBucket: "",
+    messagingSenderId: "564681059274"
+  }
 };
+
+
+
+
